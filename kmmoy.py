b779b8d95ff4beafc40fa3f7d94574cc02b30224
charisma <95167277+guepardmesa@users.noreply.github.com>
2024-03-03 22:00:24 +0100

    Update kmmoy.py with RST comments

--- kmmoy.py
+++ kmmoy.py
@@ -1,4 +1,5 @@
-"""Ce module définit la classe Voiture pour représenter les objets voitures.
+"""
+Ce module définit la classe Voiture pour représenter les objets voitures.
 
 La classe Voiture possède des attributs tels que la marque, le modèle, le kilométrage
 et la quantité de carburant utilisée. Elle offre des méthodes pour parcourir des kilomètres,
@@ -6,44 +7,53 @@ ajouter du carburant et calculer la consommation moyenne de carburant.
 """
 
 class Voiture:
-    """Classe représentant une voiture.
-
-    Attributes:
-        marque (str): La marque de la voiture.
-        modele (str): Le modèle de la voiture.
-        kilometrage (float): Le total de kilomètres parcourus.
-        carburant_utilise (float): La quantité totale de carburant utilisé.
     """
-    # Méthode d'initialisation, appelée lors de la création d'une nouvelle instance
+    Classe représentant une voiture.
+
+   :param str marque: La marque de la voiture.
+    :param str modele: Le modèle de la voiture.
+
+    :ivar marque: La marque de la voiture.
+    :ivar modele: Le modèle de la voiture.
+    :ivar float kilometrage: Le total de kilomètres parcourus, initialisé à 0.
+    :ivar float carburant_utilise: La quantité totale de carburant utilisé, initialisée à 0.
+    """
+    ## Méthode d'initialisation, appelée lors de la création d'une nouvelle instance
     def __init__(self, marque, modele):
-        self.marque = marque  # Attribut d'instance pour la marque de la voiture
-        self.modele = modele  # Attribut d'instance pour le modèle de la voiture
-        self.kilometrage = (
-            0  # Attribut d'instance pour le kilométrage initialisé à zéro
-        )
-        self.carburant_utilise = (
-            0  # Attribut d'instance pour la quantité de carburant initialisée à zéro
-        )
+
+
+        """
+        Initialisation d'une nouvelle instance de :class:`Voiture`.
+
+        :param str marque: Marque de la voiture.
+        :param str modele: Modèle de la voiture.
+        """
+    
+        self.marque = marque  
+        self.modele = modele 
+        self.kilometrage = (0)
+        self.carburant_utilise = (0)
 
     # Méthode pour ajouter des kilomètres au compteur de la voiture
     def parcourir(self, km):
-        """Ajoute des kilomètres au compteur de la voiture.
-        Args:
-            km (float): Le nombre de kilomètres à ajouter.
+        """
+        Ajoute des kilomètres au compteur de la voiture.
+
+        :param float km: Le nombre de kilomètres à ajouter.
         """
         if km < 0:
             print("Les kilomètres doivent être un nombre positif.")
         else:
-            self.kilometrage += (
-                km  # Ajout de kilomètres à l'attribut d'instance kilometrage
-            )
+            self.kilometrage += km
 
     # Méthode pour ajouter du carburant
     def ajouter_carburant(self, litres):
-        """Ajoute du carburant à la voiture.
-        Args:
-            litres (float): La quantité de carburant à ajouter.
+       """
+        Ajoute du carburant à la voiture.
+
+        :param float litres: La quantité de carburant à ajouter.
         """
+        
         if litres < 0:
             print("La quantité de carburant doit être un nombre positif.")
         else:
@@ -52,11 +62,13 @@ class Voiture:
 
     # Méthode pour calculer la consommation moyenne de gasoil
     def gasoil_burn(self):
-        """Calcule la consommation moyenne de carburant.
-
-        Returns:
-            str: La consommation moyenne de carburant formatée.
         """
+        Calcule la consommation moyenne de carburant.
+
+        :return: La consommation moyenne de carburant formatée en litres aux 100 km.
+        :rtype: str
+        """
+        
         if self.kilometrage == 0:
             return "Aucun kilomètre parcouru. Impossible de calculer la consommation."
         consommation_moyenne = (self.carburant_utilise / self.kilometrage) * 100
@@ -73,11 +85,11 @@ ma_voiture.ajouter_carburant(
 )  # Appel de la méthode ajouter_carburant pour ajouter 30 litres de carburant
 print(ma_voiture.gasoil_burn())  # Affichage de la consommation moyenne de gasoil
 
+"""
+.. comment::
 
-# Le mot-clé self en Python est utilisé dans les classes
-#pour faire référence à l'instance actuelle de la classe.
-# Il permet d'accéder et de manipuler les attributs
-# spécifiques à chaque objet créé à partir de la classe,
-# garantissant que les méthodes agissent sur les données
-# propres à chaque instance plutôt que sur des variables globales.
-# En bref, self est un moyen de se référer à l'objet actuel dans une classe.
+   Le mot-clé ``self`` en Python est utilisé dans les classes pour faire référence à l'instance actuelle de la classe.
+   Il permet d'accéder et de manipuler les attributs spécifiques à chaque objet créé à partir de la classe,
+   garantissant que les méthodes agissent sur les données propres à chaque instance plutôt que sur des variables globales.
+   En bref, ``self`` est un moyen de se référer à l'objet actuel dans une classe.
+"""
